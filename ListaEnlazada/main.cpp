77fd62a8b29f1de4fe0c67c30b118741ce436ca0
LopezJimenezCarlos <lopezcarlosuem@gmail.com>
2022-12-13 17:01:20 +0100

    ListaEnlazada

--- ListaEnlazada/main.cpp
+++ ListaEnlazada/main.cpp
@@ -58,9 +58,6 @@ public:
     bool esVacia(){
         return primero == NULL;//si el primero es null es vacia
     }
-    //funciones de EXTRACCION
-
-    //funciones de ACCESO
     int size(){
         bool existe_enlace = true;
         NodoLista* aux = primero;
@@ -68,12 +65,70 @@ public:
         while(existe_enlace == true){
             if (aux->siguiente !=NULL){
                 ++contador;
-            }else{
-                existe_enlace = false;
             }
+                else{
+                 existe_enlace = false;
+              }
         }return contador;
     }
+    //funciones de EXTRACCION
+        //Extraccion por la cabeza
+    void cortar_cabeza(){
+        NodoLista* aux = primero;
+        primero = primero ->siguiente;
+        delete aux;
+    }
+        //Extraccion por la cola
+    void saca_colas(){
+        NodoLista* aux = primero;
+        for (int i = 0; i < size()-1; i++){
+            aux = aux->siguiente;
+        }
+        NodoLista* aux2= aux->siguiente;
+        aux->siguiente = 0 ;
+        delete aux2;
+    }
+        //Extraccion de una posicion concreta
+        void eliminar_posicion(int pos){
+           NodoLista* aux = primero;
+                if (pos > size() && pos <= 0){
+                    cout "No existen tantos elementos en la lista"<<endl;
+                }
+                
+            }else{
+                for(int i = 0; i < pos-1;i++){
+                      aux = aux->siguiente;
+                }
+                    NodoLista* aux2= aux->siguiente;
+                    aux->siguiente = aux2->siguiente;
+                    delete aux2;
+            }
+     
+    //funciones de ACCESO
+        //Funcion de busqueda booleana
+        bool existe_elemento(T elemento){
+            NodoLista* aux = primero;
+            bool existe= false;
+            int contador = 0;
+            while(contador < size()){
+                if (aux->elemento == elemento){
+                    existe = true;
+                   // contador = size();
+                }else{
+                    ++contador;
+                }
+            }
+            return existe;
+        }
+        //Funcion de busqueda posicional
+
+        //Funcion de obtencion de dato por la cabeza
+
+        //Funcion de obtencion de dato por la cola
+
+        //Funcion de obtencion de dato por cualquier lado
 };
+
 
 int main() {
 
